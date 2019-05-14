ca4a5cb869ce03d6ed95c958f77bc1d3cc506464
finalvalue <finalvalue@foxmail.com>
2019-05-14 10:53:31 +0800

    Speaker_test

---
+++ Speaker-喇叭模块/Speaker_test/Speaker_test.ino
@@ -0,0 +1,29 @@
+//Speaker test
+//simulate the ambulance voice by speaker
+
+int speakerPin=6; //set speaker pin
+
+void setup() { 
+  pinMode(speakerPin,OUTPUT);
+}
+
+void loop() {
+  unsigned char i,j;
+  while(1) 
+  { 
+    for(i=0;i<80;i++) //make frequency of a sound
+    { 
+      digitalWrite(speakerPin,HIGH);
+      delay(1);
+      digitalWrite(speakerPin,LOW);
+      delay(1);
+    } 
+    for(i=0;i<100;i++) //make another frequency of a sound
+    { 
+      digitalWrite(speakerPin,HIGH);
+      delay(2); 
+      digitalWrite(speakerPin,LOW);
+      delay(2); 
+    } 
+  } 
+}
